9439680a26944105ca8b136e58472bc466665494
gates <alexandre.lecom@gmail.com>
2018-06-03 23:30:28 +0100

    fix layout produtos

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,21 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../services/product.service';
+import {map} from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
-  styleUrls: ['./product.component.css'],
+  styleUrls: ['./product.component.scss'],
   providers: [ProductService]
 })
 export class ProductComponent implements OnInit {
 
   public products = [];
+  images: Array<string>;
 
-  constructor(private _productService: ProductService) { }
+  constructor(private _productService: ProductService, private _http: HttpClient) { }
 
   ngOnInit() {
+    this._http.get('https://picsum.photos/list')
+    .pipe(map((images: Array<{id: number}>) => this._randomImageUrls(images)))
+    .subscribe(images => this.images = images);
+
     this._productService.getProducts()
     .subscribe(data => this.products = data);
   }
 
+  private _randomImageUrls(images: Array<{id: number}>): Array<string> {
+    return [1, 2, 3].map(() => {
+      const randomId = images[Math.floor(Math.random() * images.length)].id;
+      return `https://picsum.photos/300/400?image=${randomId}`;
+    });
+  }
 }
